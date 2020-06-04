07a68c85948ed09f81d2301260e5a14a751d0b70
cimyjh <cimyjh@gmail.com>
2020-06-04 10:57:58 +0900

    Initial commit

---
+++ src/main/java/com/dmc/oauth2/domain/posts/Posts.java
@@ -0,0 +1,40 @@
+package com.dmc.oauth2.domain.posts;
+
+import com.dmc.oauth2.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name= "posts")
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "posts_id")
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
