29b068465b5e27f8c0c2c817eeeb2f1b3f82b091
cimyjh <cimyjh@gmail.com>
2020-06-06 19:53:28 +0900

    EventCategorySearchCondition.java와 같이 누락된 파일들 commit

---
+++ src/main/java/com/dmc/oauth2/domain/event/dto/EventCategorySearchCondition.java
@@ -0,0 +1,6 @@
+package com.dmc.oauth2.domain.event.dto;
+
+public class EventCategorySearchCondition {
+    private String evt;
+    private String product1;
+}
---
+++ src/main/java/com/dmc/oauth2/domain/event/dto/EventNameSearchCondition.java
@@ -0,0 +1,9 @@
+package com.dmc.oauth2.domain.event.dto;
+
+import lombok.Data;
+
+@Data
+public class EventNameSearchCondition {
+    private String name;
+
+}
