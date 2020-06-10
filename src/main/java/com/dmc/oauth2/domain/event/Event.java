feccdd6831bab3990a6661f52bef5d66eb925108
cimyjh <cimyjh@gmail.com>
2020-06-10 14:06:57 +0900

    Swagger 적용 완료. 백엔드 개발 완료.

--- src/main/java/com/dmc/oauth2/domain/event/Event.java
+++ src/main/java/com/dmc/oauth2/domain/event/Event.java
@@ -1,5 +1,7 @@
 package com.dmc.oauth2.domain.event;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +11,24 @@ import java.util.Date;
 @Entity
 @Getter	@Setter
 @Table(name = "event")
+@ApiModel(description = "편의점 이벤트 상품 정보를 위한 도메인 객체")
 public class Event {
 	
 	@Id	@GeneratedValue
-	@Column(name = "event_id")	
+	@Column(name = "event_id")
+    @ApiModelProperty(notes = "이벤트 번호입니다.")
     private Long eventNum;
     
-    
+
+	@ApiModelProperty(notes = "상품명을 입력해 주세요.")
     private String name;
+
+
     private int price;
+    @ApiModelProperty(notes = "상품 이벤트 구분을 입력해 주세요.")
     private String evt;
+
+    @ApiModelProperty(notes = "상품 카테고리를 입력해 주세요.")
     private String product1;
     private String store;
     private String product2;
--- src/main/java/com/dmc/oauth2/domain/news/News.java
+++ src/main/java/com/dmc/oauth2/domain/news/News.java
@@ -1,6 +1,8 @@
 package com.dmc.oauth2.domain.news;
 
 import com.dmc.oauth2.domain.review.Review;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,14 +15,18 @@ import java.util.List;
 @Setter
 @Entity
 @Table(name = "news")
+@ApiModel(description = "편의점 신상품 상품 정보를 위한 도메인 객체")
 public class News {
 	
 	@Id @GeneratedValue
 	@Column(name = "news_id")
 	private Long newsNum;
-	
+
+	@ApiModelProperty(notes = "상품명을 입력해 주세요.")
 	private String name;
 	private int price;
+
+	@ApiModelProperty(notes = "상품 카테고리를 입력해 주세요.")
 	private String product1;
 	private String product2;
 	private String thumbnail;
