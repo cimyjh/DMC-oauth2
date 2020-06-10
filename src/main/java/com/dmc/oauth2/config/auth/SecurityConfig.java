e4699f503d0deb99b665d70266729bcf065166b2
cimyjh <cimyjh@gmail.com>
2020-06-10 16:47:01 +0900

    Swagger에 security 예외 적용 완료.

--- src/main/java/com/dmc/oauth2/config/auth/SecurityConfig.java
+++ src/main/java/com/dmc/oauth2/config/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dmc.oauth2.config.auth;
 import com.dmc.oauth2.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -21,7 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/","/v1/**" ,"/event/**","/scss/**", "/vendor/**", "/css/**", "/img/**", "/js/**", "/h2-console/**", "/profile", "/lib/**").permitAll()
+                    .antMatchers("/","/v1/**" ,"/event/**","/scss/**",
+                            "/vendor/**", "/css/**", "/img/**", "/js/**", "/h2-console/**",
+                            "/profile", "/lib/**", "/v2/api-docs", "/configuration/ui",
+                            "/swagger-resources/**", "/configuration/**", "/swagger-ui.html",
+                            "/webjars/**").permitAll()
 //                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .antMatchers("/new/**").authenticated()
                     .anyRequest().authenticated()
@@ -34,4 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .userInfoEndpoint()
                             .userService(customOAuth2UserService);
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/v1/api-docs", "/configuration/ui",
+//                "/swagger-resources", "/configuration/security",
+//                "/swagger-ui.html", "/webjars/**","/swagger/**");
+//    }
 }
