5093d85e5f2bc0cb3e1fae7cc69be24ece32756b
cimyjh <cimyjh@gmail.com>
2020-07-11 02:06:11 +0900

    Junit 5 적용 중

---
+++ src/test/java/com/dmc/oauth2/web/CommonWebControllerTest.java
@@ -0,0 +1,28 @@
+package com.dmc.oauth2.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonWebControllerTest {
+
+    @Test
+    void resume() {
+    }
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void dispLoginResult() {
+    }
+
+    @Test
+    void dispLogout() {
+    }
+
+    @Test
+    void dispDenied() {
+    }
+}
